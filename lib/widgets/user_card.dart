13afdf33837cefcd2e407152ca0c6701bb547f55
ShiryaevNikolay <deendeeps@list.ru>
2021-04-24 20:23:16 +0300

    Кастомизирует элементы списка

--- lib/widgets/user_card.dart
+++ lib/widgets/user_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:users_list_flutter/domain/models/user.dart';
 
 class UserCard extends StatelessWidget {
@@ -7,7 +8,40 @@ class UserCard extends StatelessWidget {
   final User? user;
 
   @override
-  Widget build(BuildContext context) => Row(
-        children: [Text(user!.name!)],
-      );
+  Widget build(BuildContext context) => Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+    child: Row(
+          children: [
+            SvgPicture.asset("assets/icons/ic_user.svg"),
+            const SizedBox(width: 16,),
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    user!.name!,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text(
+                    user!.email!,
+                    style: TextStyle(
+                      fontSize: 13,
+                      color: Theme.of(context).disabledColor
+                    ),
+                  ),
+                  Text(
+                    user!.company!,
+                    style: TextStyle(
+                      fontSize: 13
+                    ),
+                  ),
+                ],
+              )
+            )
+          ],
+        ),
+  );
 }
